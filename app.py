9065691a927d1e9afe7abb923381b30b059307a9
Anonmint-018 <anonmint7@gmail.com>
2026-04-25 11:22:34 +0530

    fix production run

--- app.py
+++ app.py
@@ -617,4 +617,4 @@ def api_report(report_id):
         "fir_filed_by":r["fir_filed_by"],"fir_filed_at":r["fir_filed_at"]})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
